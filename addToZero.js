0a2f355ab4c797b3fbbf37e08e88fdf6823c1b05
Lily Springer <lilyspringer@hotmail.com>
2023-01-28 16:22:53 -0700

    solved

--- addToZero.js
+++ addToZero.js
@@ -1,4 +1,17 @@
 // Starting array
-let array = [28, 43, -12, 30, 4, 0, 12]
+let array = [0, 1, 2, 3, 4, 5]
 
 // Write your solution below:
+
+let sumZero = false
+for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+        if (i !== j) {    
+            if (array[i] + array[j] === 0) {
+                sumZero = true;
+            }
+        }    
+    } 
+}    
+
+console.log(sumZero)
