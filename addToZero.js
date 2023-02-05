3c7865795dabab9db738254aa2ad372b86384982
Lily Springer <lilyspringer@hotmail.com>
2023-02-04 18:50:38 -0700

    syntax

--- addToZero.js
+++ addToZero.js
@@ -3,12 +3,12 @@ let array = [0, 1, 2, 3, 4, 5]
 
 // Write your solution below:
 
-let sumZero = false
+let isSumZero = false
 for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length; j++) {
         if (i !== j) {    
             if (array[i] + array[j] === 0) {
-                sumZero = true;
+                isSumZero = true;
             }
         }    
     } 
